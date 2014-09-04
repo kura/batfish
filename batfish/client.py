0f0c75a20878f111dc1c6984571f2e43b097351b
Kura <kura@deviling.net>
2014-09-04 21:30:01 +0100

    Moar docs

--- batfish/client.py
+++ batfish/client.py
@@ -266,7 +266,7 @@ class Client(object):
 
     def droplet_rename(self, droplet, name):
         """
-        Send an API request rename a droplet.
+        Send an API request to rename a droplet.
 
             >>> cli = batfish.Client()
             >>> cli.droplet_rename(123456, "kura-test")
@@ -307,6 +307,19 @@ class Client(object):
                         {'type': 'snapshot', 'name': name})
 
     def droplet_resize(self, droplet, size):
+        """
+        Send an API request to resize a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_resize(123456, "512MB")
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :param size: A size to resize to, either a string or an instance of
+                     `batfish.models.Size`.
+        :rtype: Dictionary of the JSON response.
+        """
         if isinstance(droplet, Droplet):
             droplet = droplet.id
         if isinstance(size, Size):
@@ -315,44 +328,196 @@ class Client(object):
                         {'type': 'resize', 'size': size})
 
     def droplet_restore(self, droplet, image):
+        """
+        Send an API request to restore a droplet from an image.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_restore(123456, 9087765)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :param image: An image ID, slug or an instance of
+                      `batfish.models.Image`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_image_action('restore', droplet, image)
 
     def droplet_rebuild(self, droplet, image):
+        """
+        Send an API request to rebuild a droplet from an image.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_rebuild(123456, 9087765)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :param image: An image ID, slug or an instance of
+                      `batfish.models.Image`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_image_action('rebuild', droplet, image)
 
     def droplet_enable_ipv6(self, droplet):
+        """
+        Send an API request to enable IPv6 on a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_enable_ipv6(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('enable_ipv6', droplet)
 
     def droplet_disable_backups(self, droplet):
+        """
+        Send an API request to disable backups for a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_disable_backups(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('disable_backups', droplet)
 
     def droplet_enable_private_networking(self, droplet):
+        """
+        Send an API request to enable private networking on a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_enable_private_networking(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('enable_private_networking', droplet)
 
     def droplet_reboot(self, droplet):
+        """
+        Send an API request to reboot a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_reboot(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('reboot', droplet)
 
     def droplet_power_cycle(self, droplet):
+        """
+        Send an API request to power cycle a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_power_cycle(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('power_cycle', droplet)
 
     def droplet_power_off(self, droplet):
+        """
+        Send an API request to power off a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_power_off(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('power_off', droplet)
 
     def droplet_power_on(self, droplet):
+        """
+        Send an API request to power on a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_power_on(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('power_on', droplet)
 
     def droplet_password_reset(self, droplet):
+        """
+        Send an API request to reset the root password of a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_password_reset(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('password_reset', droplet)
 
     def droplet_shutdown(self, droplet):
+        """
+        Send an API request to shutdown a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_shutdown(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         self.simple_droplet_action('shutdown', droplet)
 
     def droplet_delete(self, droplet):
+        """
+        Send an API request to delete a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_delete(123456)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param droplet: The droplet to modify, either a droplet ID or an
+                        instance of `batfish.models.Droplet`.
+        :rtype: Dictionary of the JSON response.
+        """
         if isinstance(droplet, Droplet):
             droplet = droplet.id
         print self.delete('droplets/{0}'.format(droplet))
 
     def droplet_create(self, name, region, size, image):
+        """
+        Send an API request to create a droplet.
+
+            >>> cli = batfish.Client()
+            >>> cli.droplet_power_off('kura-new', 'ams2', '512MB', 908765)
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param name: The name of the new droplet.
+        :param region: The region to create the droplet in. Accepts a name,
+                       slug or an instance of `batfish.models.Region`.
+        :param size: The size of the new droplet. Accepts a slug or instance
+                     of `batfish.models.Size`.
+        :param image: An image to build the droplet from. Accepts a name, slug
+                      or an instance of `batfish.models.Image`.
+        :rtype: Dictionary of the JSON response.
+        """
         if isinstance(size, Size):
             size = size.slug
         if isinstance(image, Image):
@@ -370,12 +535,31 @@ class Client(object):
 
     @property
     def images(self):
+        """
+        Get a list of images from the API.
+
+            >>> cli = batfish.Client()
+            >>> cli.images
+            [<Image test1>, <Image test2>]
+
+        :rtype: List of `batfish.models.Image` objects.
+        """
         j = self.get('images')
         if 'images' not in j:
             return None
         return [Image(i) for i in j['images']]
 
     def image_from_id(self, image_id):
+        """
+        Get an instance of `batfish.models.Image` for the provided image ID.
+
+            >>> cli = batfish.Client()
+            >>> cli.image_from_id(12345)
+            <Image test1>
+
+        :param image_id: An integer ID of an image.
+        :rtype: An instance of `batfish.models.Image`.
+        """
         url = "images/{0}".format(image_id)
         j = self.get(url)
         if 'image' not in j:
@@ -383,6 +567,16 @@ class Client(object):
         return Image(j['image'])
 
     def image_from_name(self, name):
+        """
+        Get an instance of `batfish.models.Image` for the provided image name.
+
+            >>> cli = batfish.Client()
+            >>> cli.image_from_name('test1')
+            <Image test1>
+
+        :param name: A string name of an image.
+        :rtype: An instance of `batfish.models.Image`.
+        """
         j = self.get('images')
         if 'images' not in j:
             return None
@@ -392,6 +586,16 @@ class Client(object):
         return None
 
     def image_from_slug(self, slug):
+        """
+        Get an instance of `batfish.models.Image` for the provided image slug.
+
+            >>> cli = batfish.Client()
+            >>> cli.image_from_slug('test1')
+            <Image test1>
+
+        :param slug: A string slug of an image.
+        :rtype: An instance of `batfish.models.Image`.
+        """
         url = "images/{0}".format(slug.lower())
         j = self.get(url)
         if 'image' not in j:
@@ -399,11 +603,32 @@ class Client(object):
         return Image(j['image'])
 
     def image_delete(self, image):
+        """
+        Send an API request to delete an image.
+
+            >>> cli = batfish.Client()
+            >>> cli.image_delete(123456)
+
+        :param image: The string name, slug, integer ID of an image or an
+                      instance of `batfish.models.Image`.
+        """
         if isinstance(image, Image):
             image = image.id
         print self.delete('images/{0}'.format(image))
 
     def image_rename(self, image, name):
+        """
+        Send an API request to rename an image.
+
+            >>> cli = batfish.Client()
+            >>> cli.image_rename(90876, "kura-test")
+            {'response': "Nothing", 'reason': "Meh."}
+
+        :param image: The image to modify, either a image ID, name, slug or an
+                        instance of `batfish.models.Image`.
+        :param name: A string of the new name.
+        :rtype: Dictionary of the JSON response.
+        """
         if isinstance(image, Image):
             image = image.id
         if not valid_chars.match(name):
